e525203656ef3c27d8da4eafcbf708d41cb42982
Rondi650 <rondinellenunes123@gmail.com>
2026-05-17 19:39:34 -0300

    teste e inclusao de arquivo temporario

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from recipes.views import home
+from django.urls import path
+from recipes.views import home, temp
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('temp/', temp),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request,
+                  template_name='recipes/home.html',
+                  context={'nome': 'Rondi Oliveira'},
+                  status=202)
+
+
+def temp(request):
+    return render(request,
+                  template_name='temp/temp.html',
+                  context={'titulo': 'Teste Temporario'},
+                  status=202)
