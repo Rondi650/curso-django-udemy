08496f5fbdc9de306dcbf1a650a228dd08efbb7d
Rondi650 <rondinellenunes123@gmail.com>
2026-05-23 14:53:22 -0300

    estudo do arquivo de settings e higienizacao

--- projeto/settings.py
+++ projeto/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +57,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'base_templates',
-            BASE_DIR / 'recipes' / 'templates_temp'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
