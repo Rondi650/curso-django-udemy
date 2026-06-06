92fad8e0b669985c7fc757670e984624e5801c13
Rondi650 <rondinellenunes123@gmail.com>
2026-06-06 17:29:36 -0300

    ajuste do script que sobe para o container

--- scripts/comands.sh
+++ scripts/comands.sh
@@ -2,5 +2,6 @@
 
 echo "Running Django development server on port 12345..."
 python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:12345
