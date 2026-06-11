06cff5f7aa392712c73eb835127ad9ca06d2ac14
Rondi650 <rondinellenunes123@gmail.com>
2026-06-11 18:20:44 -0300

    adiciona campo de categoria e autor ao modelo de receita; atualiza views para usar dados do banco

--- recipes/views.py
+++ recipes/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
 from utils._faker import make_recipe
+from recipes.models import Recipe
 
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request,
                   template_name='recipes/pages/home.html',
                   context={
                       'page_title': 'Home',
-                      'recipes': [make_recipe() for _ in range(10)]
+                      'recipes': recipes
                   }
                   )
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(id)
     return render(request,
                   template_name='recipes/pages/recipe-view.html',
                   context={
                       'page_title': 'Recipes',
-                      'recipe': make_recipe(),
+                      'recipe': recipe,
                       'is_detail_page': True
                   }
                   )
