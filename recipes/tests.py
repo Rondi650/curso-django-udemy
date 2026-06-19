3346713bdd9afe980e2847f2b6463a26be7b4129
Rondi650 <rondinellenunes123@gmail.com>
2026-06-18 21:47:22 -0300

    adiciona configurações de teste no settings.json e pytest.ini; implementa testes básicos em recipes/tests.py

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
+import pytest
 
 # Create your tests here.
 
 
+@pytest.mark.slow
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
+        print('bom dia')
         assert 1 == 1, 'Um é igual a um'
+
+
+class Test_erro(TestCase):
+    def test_erro(self):
+        assert 1 == 2
