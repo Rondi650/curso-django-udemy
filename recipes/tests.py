57b33b4e7172a6f8df20d15316a2bccfbcfbecca
Rondi650 <rondinellenunes123@gmail.com>
2026-06-21 12:11:37 -0300

    refactor tests: update test structure in recipes/tests.py for clarity; enhance pytest.ini options

--- recipes/tests.py
+++ recipes/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 import pytest
 
-# Create your tests here.
+# Unittest Django
 
 
 class RecipeURLTest(TestCase):
@@ -10,8 +10,17 @@ class RecipeURLTest(TestCase):
         url = reverse('recipes:home')
         assert url == '/'
 
+    def test_recipe_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        assert url == '/recipes/1/'
 
+    def test_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        assert url == '/recipes/category/1/'
+
+
+# pytest enviando multiplos ids em loop
 @pytest.mark.parametrize('category_id', [1, 2, 3])
-def test_category_url_is_correct(category_id):
+def test_category_multiple_ids_url_is_correct(category_id):
     url = reverse('recipes:category', kwargs={'category_id': category_id})
     assert url == f'/recipes/category/{category_id}/'
