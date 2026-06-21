a077524612bf9debb5edca01a88ecfdf60907160
Rondi650 <rondinellenunes123@gmail.com>
2026-06-21 11:34:17 -0300

    refactor tests: remove verbosity option from pytest.ini; improve test structure in recipes/tests.py

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
+import pytest
 
 # Create your tests here.
 
 
 class RecipeURLTest(TestCase):
     def test_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        assert home_url == '/'
+        url = reverse('recipes:home')
+        assert url == '/'
+
+
+@pytest.mark.parametrize('category_id', [1, 2, 3])
+def test_category_url_is_correct(category_id):
+    url = reverse('recipes:category', kwargs={'category_id': category_id})
+    assert url == f'/recipes/category/{category_id}/'
