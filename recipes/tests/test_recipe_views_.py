775249338ca5f68892e5334afce7a126cc5d65dc
Rondi650 <rondinellenunes123@gmail.com>
2026-06-30 14:02:50 -0300

    refactor tests: streamline recipe view tests by introducing response_get_recipe helper method

--- recipes/tests/test_recipe_views_.py
+++ recipes/tests/test_recipe_views_.py
@@ -131,35 +131,34 @@ class RecipeCategoryViewDataTest(RecipeTestBase):
 
 
 class RecipeViewDataTest(RecipeTestBase):
+    def response_get_recipe(self, id=1):
+        return self.client.get(
+            reverse('recipes:recipe', kwargs={'id': id}))
+
     def test_recipe_template_loads_recipes(self):
         self.make_recipe(category={'name': 'pao'})
-        response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.response_get_recipe()
         response_content = response.context['recipe']
         self.assertEqual(response_content.id, 1)
 
     def test_recipe_status_code_200(self):
         self.make_recipe(category={'name': 'pao'})
-        response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.response_get_recipe()
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_status_code_404(self):
-        response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.response_get_recipe(id=999)
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_template_loads_altered(self):
         self.make_recipe(category={'name': 'Especial'})
-        response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.response_get_recipe()
         response_content = response.context['recipe']
         self.assertEqual(response_content.category.name, 'Especial')
 
     def test_recipe_template_has_data(self):
         self.make_recipe()
-        response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.response_get_recipe()
         content = response.content.decode('utf-8')
         self.assertIn('Nova Categoria', content)
 
