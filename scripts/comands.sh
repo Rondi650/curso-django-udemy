470166ab673fbbf7de84586d30031b22cb68dabf
Rondi650 <rondinellenunes123@gmail.com>
2026-07-05 22:50:57 -0300

    chore: update collectstatic command to include --clear option

--- scripts/comands.sh
+++ scripts/comands.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Running Django development server on port 12345..."
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput --clear
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:12345
