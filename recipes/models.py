4607a7f326f6d0313104e19ee9db31008ed05824
Rondi650 <rondinellenunes123@gmail.com>
2026-07-28 13:41:15 -0300

    add(models): enhance shell commands for duplicating, renaming, and deleting Recipe instances

--- recipes/models.py
+++ recipes/models.py
@@ -41,3 +41,73 @@ CRIANDO USER NO DJANGO SHELL (exemplo)
 >>> User.objects.create_user(first_name='XPTO',last_name='C3PO', 
 username='pqp_pra_la',password='jafsyuhasfyfas1524')
 """
+
+'''
+DUPLICANDO RECEITAS PELO SHELL
+>>> import random
+>>> for i in r:
+...     i.id = None  # Reseta o ID para forçar a criação de um novo registro
+...     sufixo = random.randint(100, 999)  # Gera um número aleatório
+...     i.slug = f"{i.slug}-{sufixo}"        # Concatena como string (ex: 'bolo-de-pote-482')
+...     i.save()
+'''
+
+
+'''
+RENOMEANDO SLUGS
+>>> r = Recipe.objects.all()
+>>> r
+<QuerySet [<Recipe: Bolo de Pote>, <Recipe: Almondega Recheada>, <Recipe: Frango com Quiabo>, <Recipe: Bolo de chololate>, <Recipe: Pao de Lo>]>
+>>> for i in r: p = re.search(r'[a-z]+(?:-[a-z]+)*',i.slug); print(p)
+... 
+<re.Match object; span=(0, 12), match='bolo-de-pote'>
+<re.Match object; span=(0, 17), match='almondega-maneira'>
+<re.Match object; span=(0, 17), match='frango-com-quiabo'>
+<re.Match object; span=(0, 17), match='bolo-de-chocolate'>
+<re.Match object; span=(0, 9), match='pao-de-lo'>
+>>> for i in r: p = re.search(r'[a-z]+(?:-[a-z]+)*',i.slug); print(p.group())
+... 
+bolo-de-pote
+almondega-maneira
+frango-com-quiabo
+bolo-de-chocolate
+pao-de-lo
+>>> for i in r: p = re.search(r'[a-z]+(?:-[a-z]+)*',i.slug); i.slug = p.group()
+... 
+>>> for i in r: print(i.slug)
+... 
+bolo-de-pote
+almondega-maneira
+frango-com-quiabo
+bolo-de-chocolate
+pao-de-lo
+
+'''
+
+
+'''
+DELETANDO IDS PELO SHELL
+>>> for i in r:
+...     if i.id >=8:
+...             i.delete()
+... 
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+(1, {'recipes.Recipe': 1})
+>>> r = Recipe.objects.all()  # Força nova consulta no banco
+>>> r
+<QuerySet [<Recipe: Bolo de Pote>, <Recipe: Almondega Recheada>, <Recipe: Frango com Quiabo>, <Recipe: Bolo de chololate>, <Recipe: Pao de Lo>]>
+>>> len(r)
+5
+
+'''
