c9e5c39991a08e842f91d7843babde4b361336c5
Rondi650 <rondinellenunes123@gmail.com>
2026-08-09 14:51:06 -0300

    fix(tests): update search query parameter and add test for escaped search term in title

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -50,13 +50,22 @@ class RecipeViewsTest(TestCase):
         self.assertIs(view.func, views.search)
 
     def test_recipe_search_view_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search') + '?q=teste')
+        response = self.client.get(reverse('recipes:search') + '?search=teste')
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
         url = reverse('recipes:search')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+        
+        
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?search=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Resultados para: &lt;Teste&gt;',
+            response.content.decode('utf-8')
+        )
 
 
 class RecipeHomeViewDataTest(RecipeTestBase):
