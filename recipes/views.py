ce83744675f9caca0ee008877e914b4fc45a627b
Rondi650 <rondinellenunes123@gmail.com>
2026-08-15 11:09:06 -0300

    refactor(search): improve readability of search query by formatting Q objects

--- recipes/views.py
+++ recipes/views.py
@@ -60,8 +60,10 @@ def search(request: HttpRequest):
         raise Http404()
 
     recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) |
-        Q(description__icontains=search_term),
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term)
+        ),
         is_published=True
     ).order_by('-id')
 
