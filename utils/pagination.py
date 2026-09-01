21c660c19efa2096dfaa3c9e15372679b4960c7a
Rondi650 <rondinellenunes123@gmail.com>
2026-09-01 20:26:41 -0300

    style(pagination): add newlines for improved readability

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,6 +2,7 @@ import math
 from typing import Any
 from django.core.paginator import Paginator
 
+
 def make_pagination_range(
     page_range,
     qty_pages,
@@ -35,6 +36,7 @@ def make_pagination_range(
         'last_page_out_of_range': stop_range < total_pages,
     }
 
+
 def make_pagination(request, queryset, per_page, qty_pages=4):
     try:
         current_page = int(request.GET.get('page', 1))
@@ -51,3 +53,9 @@ def make_pagination(request, queryset, per_page, qty_pages=4):
     )
 
     return page_obj, pagination_range
+
+
+# python -c
+# "import string as s;from random import SystemRandom as
+# sr;print(''.join(sr().choices(s.ascii_letters +
+# s.punctuation, k=64)))"
