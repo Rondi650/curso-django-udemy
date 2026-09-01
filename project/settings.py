6d0366cf43edf89a6835f03e720bbea3777f84b3
Rondi650 <rondinellenunes123@gmail.com>
2026-09-01 12:07:23 -0300

    feat(env): load environment variables from .env file in manage.py, asgi.py, and wsgi.py

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^&lgttlo)0$=vq_reu7x3!)9z#*9+(xxi(vvycxw$d&fvcmf@n'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = ['recipes.docker.localhost']
 
