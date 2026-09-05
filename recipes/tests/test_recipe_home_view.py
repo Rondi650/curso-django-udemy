08b4eaaff8f389a64e7a75d47879b79575546b1d
Rondi650 <rondinellenunes123@gmail.com>
2026-09-05 11:17:48 -0300

    test(pagination): add unit test for recipe home view pagination

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -2,6 +2,7 @@ from django.urls import resolve, reverse
 from recipes import views
 from recipes.tests.test_base import RecipeTestBase
 from pprint import pprint
+from unittest.mock import patch
 
 
 class RecipeHomeViewDataTest(RecipeTestBase):
@@ -95,3 +96,18 @@ class RecipeHomeViewDataTest(RecipeTestBase):
         self.assertIn(
             'Sem receitas disponibilizadas aqui.',
             content)
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(8):
+            kwargs = {'slug': f'r{i}', 'author': {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGES', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 2)
