5a32f69d80383b4a19ec1aea49fd460c8a30bb8f
Rondi650 <rondinellenunes123@gmail.com>
2026-09-09 19:42:55 -0300

    fix(settings): add custom message tags for message framework

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,11 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'  # montagem da url
 MEDIA_ROOT = BASE_DIR / 'media/'  # caminho fisico dos dados
+
+MESSAGES_TAGS = {
+    constants.DEBUG: 'message-debug',
+    constants.ERROR: 'message-error',
+    constants.INFO: 'message-info',
+    constants.SUCCESS: 'message-success',
+    constants.WARNING: 'message-warning',
+}
