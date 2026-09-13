8aee90957db8666e7e06ae6d0df0340979199370
Rondi650 <rondinellenunes123@gmail.com>
2026-09-13 13:23:56 -0300

    feat(authors): implement user registration form and update registration view

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from authors.forms import RegisterForm
 
 # Create your views here.
 
 
 def register_view(request):
+    form = RegisterForm()
     return render(request,
-                  template_name='author/pages/register_view.html')
+                  template_name='author/pages/register_view.html',
+                  context={
+                      'form': form
+                  })
