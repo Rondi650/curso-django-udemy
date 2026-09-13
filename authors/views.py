64320b18ea4dda92283137c5e7d02d3f78f5dc18
Rondi650 <rondinellenunes123@gmail.com>
2026-09-13 13:03:31 -0300

    feat(authors): add author registration view and URL routing

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def register_view(request):
+    return render(request,
+                  template_name='author/pages/register_view.html')
